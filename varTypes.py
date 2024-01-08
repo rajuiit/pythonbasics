1638b1bc1cd3cb5c89808aa46d1478051b93a2ed
Md Mahfujur Rahman <mdmahfujurrahman@Mds-MacBook-Pro-2.local>
2024-01-08 11:51:26 +1000

    python basic variables

---
+++ varTypes.py
@@ -0,0 +1,15 @@
+from ctypes import cast
+
+
+x = 4
+f = 3.1416
+name = "Python"
+combination = name + " " + name
+
+#f-string or interpolation
+print(f"The combination is: {combination}")
+
+sum = x+int(f)
+
+#f-string or interpolation
+print(f"The summation is: {sum}")
