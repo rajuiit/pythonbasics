4b0d7c5f4447159d3fe135d58978cfb7de984bca
Md Mahfujur Rahman <mdmahfujurrahman@Mds-MacBook-Pro-2.local>
2024-01-09 17:40:56 +1000

    function day time added

--- functionsInPython/functions.py
+++ functionsInPython/functions.py
@@ -1,8 +1,26 @@
 import datetime
+#general date time
 def currentYear():
     date = datetime.date.today()
     year = date.year
     print(date)
     print(year)
-    
+
+    now = datetime.datetime.now()
+    currentTime = now.strftime("%H:%M:%S")
+    print("Current Time:", currentTime)
 currentYear()
+
+
+from datetime import date
+
+# date object of today's date
+def dayMonthYear():
+    today = date.today() 
+    print("Current year:", today.year)
+    print("Current month:", today.month)
+    print("Current day:", today.day)
+dayMonthYear()
+
+
+#local timezone
