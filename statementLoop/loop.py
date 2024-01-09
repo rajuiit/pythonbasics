cfcae46a15af03f6aacf17641dfcc3764bf2b8e6
Md Mahfujur Rahman <mdmahfujurrahman@Mds-MacBook-Pro-2.local>
2024-01-09 16:58:35 +1000

    Loop and nested loop

---
+++ statementLoop/loop.py
@@ -0,0 +1,24 @@
+#General for loop
+city = ['Tokyo','New York','Dhaka','Brisbane']
+for i in city:
+    print(f"city: {i} \n")
+
+numbers = [1,2,4,5,6,7,8,9]
+for i in numbers:
+    print(f"Numbers: {i} \n")
+
+#Nested for loop
+
+lists = [[1,2,3], [4,5,6], [5,7,8], [9,8,7]]
+
+for outer_i in lists:
+    for inner_j in outer_i:
+        print(f"Show inner elements: {inner_j}")
+
+#If you want to access the outer index in the nested loop, you can use the enumerate() function. 
+#This function adds a counter to an iterable and returns it. The counter increments each time the function is called.
+
+for outer_index, outer_list in enumerate(lists):
+    print("Outer list index:", outer_index)
+    for inner_element in outer_list:
+        print(inner_element)
